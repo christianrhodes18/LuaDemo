be12a0189dd3128f044ed60a13228945450864d8
Christian Rhodes <christian.rhodes@uvm.edu>
2021-03-29 21:28:25 -0400

    added main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: thech
+-- Date: 3/29/2021
+-- Time: 9:22 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+print("Hello World")
+
